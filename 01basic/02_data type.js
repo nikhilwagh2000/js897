fe03a78596f6c61ce99a3858d2b58eaa7a1fcd80
Nikhil wagh <91803314+Clg897@users.noreply.github.com>
2023-09-18 19:45:01 +0000

    data type

---
+++ 01basic/02_data type.js
@@ -0,0 +1,30 @@
+"use strict"  //treat all java script code in new version 
+
+// alert(3+3) we are using node js not a browser
+
+console.log (3
+       +3)   // code readability 
+
+
+console.log("nikhil")
+
+let name = "nikhil"
+let age = 15
+let isloggedin = false
+let state; 
+
+//******************** premative data type ###################
+ 
+// number => 2 to power 54 
+// big int => 
+// string => ""
+//boolean => true or flase 
+// null => standaloan value
+//undifined value => 
+
+
+// object 
+ 
+  console.log( typeof undefined);//  undefined 
+
+  console.log( typeof null); // object
