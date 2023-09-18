139ae1bc38205cc895c38f42428d822db79db82f
Nikhil wagh <91803314+Clg897@users.noreply.github.com>
2023-09-18 21:20:23 +0000

    conversion of data type

---
+++ 01basic/03_coversionopration.js
@@ -0,0 +1,28 @@
+let score ="100d"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // not a number   NaN
+
+// "33" => 33
+// "33d" => NaN - nan of number 
+// true => 1 / false => 0
+
+
+let isloggedin = 1    // this variable boolean or not cheak as # Boolean variable (B) always capital
+ 
+let booleanisloggedin = Boolean(isloggedin)// we are cheak here in boolean 
+//console.log(booleanisloggedin);
+
+// 1=> true ; 0=> false
+// "" => false 
+// "nikhil"=>  true
+
+
+let sameNumber = 33 
+let stringNumber = String(sameNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
