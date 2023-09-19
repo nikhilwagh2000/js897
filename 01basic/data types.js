e830c8c3cd67eb80909ebbf18ede2a53e2a45b76
Nikhil wagh <91803314+Clg897@users.noreply.github.com>
2023-09-19 16:05:21 +0000

    data type obj

---
+++ 01basic/data types.js
@@ -0,0 +1,30 @@
+// primitive data type or as we call 
+
+// 7 type : string , Number, boolean, null , undefined, bigint, symbol
+
+const score =300
+const allScore =300.2
+const isloggedin =false 
+const otherSideTemp = null 
+let email; 
+const id =Symbol('456')
+const antherId = ('456')
+
+console.log(Symbol===antherId);
+//refernce  data type  (non primitive data type)
+
+// arry , obeject, function 
+
+const  nik = [pune , nagpur, nashik, ]; //=== array always squre bracket 
+
+let myObj = {
+    name = "nikhil",
+    age = 45,
+
+}
+
+const myFunction = function(){
+
+    
+}  // fuction define  is paranthises or curly bracket
+
